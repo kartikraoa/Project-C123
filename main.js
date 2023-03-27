19825db6df6be004916bfb939a9586e0b8bde35f
kartikraoa <112100870+kartikraoa@users.noreply.github.com>
2023-03-27 17:39:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload()
+{
+}
+function setup()
+{
+    canvas=createCanvas(640,480);
+    canvas.position(250,250);
+}
+function draw()
+{
+
+}
+function onclick()
+{
+    save('#filtered image.png')
+}
